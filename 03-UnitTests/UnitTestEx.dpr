d6901fc78277a969963c1b3851e40bbffa09825f
Holger Flick <holger@flixengineering.com>
2023-02-20 12:50:24 -0500

    added unit test

---
+++ 03-UnitTests/UnitTestEx.dpr
@@ -0,0 +1,15 @@
+﻿program UnitTestEx;
+
+uses
+  Vcl.Forms,
+  WEBLib.Forms,
+  uMain in 'uMain.pas',
+  uExamples in 'uExamples.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.Run;
+end.
