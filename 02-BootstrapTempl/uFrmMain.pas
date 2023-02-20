d8549b64c28911b20efb1ad211a5dc25d2767c40
Holger Flick <holger@flixengineering.com>
2023-02-20 12:50:17 -0500

    added bootstrap ex

---
+++ 02-BootstrapTempl/uFrmMain.pas
@@ -0,0 +1,25 @@
+﻿unit uFrmMain;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, JS, Web, WEBLib.Graphics, WEBLib.Controls,
+  WEBLib.Forms, WEBLib.Dialogs, Vcl.Controls, Vcl.StdCtrls, WEBLib.StdCtrls;
+
+type
+  TForm1 = class(TWebForm)
+    WebEdit1: TWebEdit;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
